429b395c555e40d4fbdac7ed5ef9fe7ef2943034
kenwest <ken.west@internode.on.net>
2016-09-18 17:34:46 +1000

    Rename node view alter hook

--- template.php
+++ template.php
@@ -9,7 +9,7 @@
  * When viewing a node in 'full' mode, show contextual links. The navigation
  * tabs are hidden by CSS.
  */
-function logoslive_node_view_alter(&$build) {
+function smith_node_view_alter(&$build) {
   if ($build['#view_mode'] == 'full' && $build['#entity_type'] == 'node' && isset($build['#node']) && !empty($build['#node']->nid)) {
     $build['#contextual_links']['node'] = array('node', array($build['#node']->nid));
   }
