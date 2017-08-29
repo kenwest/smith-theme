48b383e5b67686f6a56426482ce1e8670c93b9e6
kenwest <ken.west@internode.on.net>
2017-08-29 19:08:51 +1000

    Never display the primary region and make the content and secondary regions 8:4 on screens larger than mobiles

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -125,13 +125,7 @@
 
   <div class="row">
 
-    <?php if (!empty($page['sidebar_first'])): ?>
-      <aside class="col-sm-3" role="complementary">
-        <?php print render($page['sidebar_first']); ?>
-      </aside>  <!-- /#sidebar-first -->
-    <?php endif; ?>
-
-    <section<?php print $content_column_class; ?>>
+    <section class="col-sm-8">
       <?php if (!empty($page['highlighted'])): ?>
         <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
       <?php endif; ?>
@@ -156,7 +150,7 @@
     </section>
 
     <?php if (!empty($page['sidebar_second'])): ?>
-      <aside class="col-sm-3" role="complementary">
+      <aside class="col-sm-4" role="complementary">
         <?php print render($page['sidebar_second']); ?>
       </aside>  <!-- /#sidebar-second -->
     <?php endif; ?>
