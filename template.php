6aa7232f52e29840ec7789f37f5e19fc5504608b
kenwest <ken.west@internode.on.net>
2017-10-04 10:06:35 +1100

    Use JS rather than CSS to ensure element heights are a multiple of line-height-computed

--- template.php
+++ template.php
@@ -22,3 +22,25 @@ function smith_entity_property($variables) {
   return cbf_entity_property($variables);
 }
 
+/*
+ * Ensure that all elements that have their font-size set have a height which is a multiple of 20px (@line-height-computed)
+ */
+function smith_preprocess_page(&$variables) {
+  $script = '
+    jQuery(document).ready( function() {
+      function standardiseHeight(elements) {
+        elements.each( function() {
+          var height = jQuery(this).height();
+          var fraction = height % 20;
+          if (jQuery(this).css("font-size") !== "14px" && fraction !== 0) {
+            jQuery(this).css("min-height",  height - fraction + 20);
+          }
+          else {
+            standardiseHeight(jQuery(this).children().not("iframe, form"));
+          }
+        });
+      };
+      standardiseHeight(jQuery(".main-container"));
+    });';
+  drupal_add_js($script, 'inline');
+}
