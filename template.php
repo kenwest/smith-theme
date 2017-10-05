1ce5c7cd707de07408f66a363817f722491b4aad
kenwest <ken.west@internode.on.net>
2017-10-05 11:48:25 +1100

    Standardise the height of elements when the window is resized

--- template.php
+++ template.php
@@ -27,20 +27,28 @@ function smith_entity_property($variables) {
  */
 function smith_preprocess_page(&$variables) {
   $script = '
+    function cbfStandardiseHeight(elements) {
+      elements.each( function() {
+        var height = jQuery(this).outerHeight(false);
+        var fraction = height % 20;
+        if (jQuery(this).css("font-size") !== "14px" && fraction !== 0) {
+          jQuery(this).css("min-height",  height - fraction + 20).addClass("cbf-standard-height");
+        }
+        else {
+          cbfStandardiseHeight(jQuery(this).children().not("iframe, form"));
+        }
+      });
+    };
+    function cbfStandardiseHeights() {
+      jQuery(".cbf-standard-height").css("min-height", "").removeClass("cbf-standard-height");
+      cbfStandardiseHeight(jQuery(".main-container, .smith-byline"));
+    };
     jQuery(document).ready( function() {
-      function standardiseHeight(elements) {
-        elements.each( function() {
-          var height = jQuery(this).outerHeight(false);
-          var fraction = height % 20;
-          if (jQuery(this).css("font-size") !== "14px" && fraction !== 0) {
-            jQuery(this).css("min-height",  height - fraction + 20);
-          }
-          else {
-            standardiseHeight(jQuery(this).children().not("iframe, form"));
-          }
-        });
-      };
-      standardiseHeight(jQuery(".main-container, .smith-byline"));
-    });';
+      cbfStandardiseHeights();
+    });
+    jQuery(window).resize( function() {
+      cbfStandardiseHeights();
+    });
+  ';
   drupal_add_js($script, 'inline');
 }
